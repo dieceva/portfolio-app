314a6b1dc3be963deb13dc1c4a44ded5e37a6353
dieceva <dieceva96@gmail.com>
2024-09-23 20:52:39 -0700

    changed App structure into components

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,20 @@ import bg1 from './assets/bg1.jpg';
 import bg2 from './assets/bg2.jpg';
 import bg3 from './assets/bg3.jpg';
 import { useRef } from 'react';
+import Navbar from './components/Navbar.jsx';
+import Info from './components/Info.jsx';
 
 import './App.css'
-import Loading from './Loading.jsx';
+import Loading from './components/Loading.jsx';
 import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import ParallaxCont from './components/ParallaxCont.jsx';
 
 
 function App() {
   return (
     <>
-    <div>
-      <Parallax pages={4}>
-        <ParallaxLayer
-          offset={0}
-          speed={1}
-          factor={1}
-          style={{
-            backgroundImage: `url(${bg3})`,
-            backgroundSize: 'cover',
-          }}
-        />
-
-        <ParallaxLayer>
-          <img src={viteLogo} />
-        </ParallaxLayer>
-
-        <ParallaxLayer onClick={() => ref.current.scrollTo(3)}
-        >
-          <h2>Welcome to my website</h2>
-        </ParallaxLayer>
-
-        <ParallaxLayer onClick={() => ref.current.scrollTo(0)}
-        >
-          <h2>Hi Mom!</h2>
-        </ParallaxLayer>
-      </Parallax>
-    </div>
+    <Navbar/>
+    <ParallaxCont/>
     </>
   );
 }
