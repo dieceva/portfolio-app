7bce8f500e1cc2aa5cb1613fed98d89aa652edbf
dieceva <dieceva96@gmail.com>
2024-09-26 03:28:07 -0700

    created component for Contact Information

---
+++ src/components/Info.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const Info = () => {
+return(
+    <>
+<div className="p-10 rounded w-4/6 bg-gray-950">
+  <div className="px-4 sm:px-0">
+    <p className=" font-semibold text-light text-5xl">Contact Information</p>
+    <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and expectations.</p>
+  </div>
+  <div className="mt-6 border-t border-gray-100">
+    <dl className="divide-y divide-gray-100">
+      <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt className="text-sm font-medium leading-6 text-light">Full name</dt>
+        <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">Diego Cevallos</dd>
+      </div>
+      <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt className="text-sm font-medium leading-6 text-light">Applying for</dt>
+        <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">Front End Trainee, Back End Trainee, Junior Developer </dd>
+      </div>
+      <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt className="flex items-center text-sm font-medium leading-6 text-light">Email address</dt>
+        <div className="flex flex-row items-center justify-between sm:grid-cols-2">
+            <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">dieceva96@gmail.com</dd>
+           
+            
+            </div>
+            <button >Contact me!</button>
+        </div>
+      <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt className="text-sm font-medium leading-6 text-white">Salary expectation</dt>
+        <dd className="mt-1 text-sm leading-6 text-white-700 sm:col-span-2 sm:mt-0">22/hr</dd>
+      </div>
+      <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+        <dt className="text-sm font-medium leading-6 text-light">Attachments</dt>
+        <dd className="mt-2 text-sm text-light sm:col-span-2 sm:mt-0">
+          <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
+            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+              <div className="flex w-0 flex-1 items-center">
+                <svg className="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" />
+                </svg>
+                <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                  <span className="truncate font-medium">resume_develper_diego.pdf</span>
+                  <span className="flex-shrink-0 text-gray-400">2.4mb</span>
+                </div>
+              </div>
+              <div className="ml-4 flex-shrink-0">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+              </div>
+            </li>
+            <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+              <div className="flex w-0 flex-1 items-center">
+                <svg className="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" />
+                </svg>
+                <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                  <span className="truncate font-medium">coverletter_front_end_developer.pdf</span>
+                  <span className="flex-shrink-0 text-gray-400">4.5mb</span>
+                </div>
+              </div>
+              <div className="ml-4 flex-shrink-0">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+              </div>
+            </li>
+          </ul>
+        </dd>
+      </div>
+    </dl>
+  </div>
+</div>
+</>
+)
+}
+export default Info
